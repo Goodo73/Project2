ddc04feb82f35d70eca8d127ed2595b26f1da20e
Mike Goodwin <goodo73@gmail.com>
2015-04-14 23:17:58 +1000

    Load book list with AJAX

--- app.rb
+++ app.rb
@@ -12,8 +12,19 @@ end
 
 get '/' do
 	@books = Book.all
+	# @genres = Genre.all
 
   erb :index
+end
+
+# get '/book/knights' do
+# 	@data = "Knights"
+# end
+
+get '/api/books' do
+	books = Book.all
+	content_type :json
+	books.to_json
 end
 
 get '/about' do
