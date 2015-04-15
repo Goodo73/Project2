ac1e85f2e058f9d4233ac47db693adbbf2ea5353
Mike Goodwin <goodo73@gmail.com>
2015-04-15 19:08:07 +1000

    Add READ and DELETE functionality

--- app.rb
+++ app.rb
@@ -16,19 +16,25 @@ end
 
 get '/' do
 	@books = Book.all
-	# @genres = Genre.all
+	@genres = Genre.all
+	@formats = Format.all
+	@categories = Category.all
 
   erb :index
 end
-
-# get '/book/knights' do
-# 	@data = "Knights"
-# end
 
 get '/api/books' do
 	books = Book.all
 	content_type :json
 	books.to_json
+end
+
+get '/api/books/:id' do
+	book = Book.find(params[:id])
+	content_type :json
+	book.to_json(:include => [:genre, :format, :category])
+
+	# binding.pry
 end
 
 get '/api/genres' do
@@ -38,17 +44,39 @@ get '/api/genres' do
 end
 
 get '/api/formats' do
-	genres = Format.all
+	formats = Format.all
 	content_type :json
-	genres.to_json
+	formats.to_json
 end
 
 get '/api/categories' do
-	genres = Category.all
+	categories = Category.all
 	content_type :json
-	genres.to_json
+	categories.to_json
+end
+
+delete '/books/:id/delete' do
+	Book.find(params[:id]).delete
+
+	redirect to '/'
+end
+
+post '/api/books' do
+	book = Book.new
+	book.title = # params[:title]
+	book.author = #
+	book.genre = #
+	book.format = #
+	book.category = #
+	book.loaned_to = #
+	book.save
+
+	content_type :json
+	book.to_json
 end
 
 get '/about' do
 	erb :about
 end
+
+
